c5d7bb867b152961fb2b3a8a05303efc9f584576
Nick Bourgeois <77581387+Nick676191@users.noreply.github.com>
2026-06-21 12:54:32 -0500

    Created folder to check assumptions and also changed end folder for parquet file

---
+++ assumptions/pois_check.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# The general shape of the poisson distribution will be right skewed to favor smaller values
+
+
+# Generally the Poisson Distribution usually has a mean that is roughly equal to the variance
